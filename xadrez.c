7ddd6a24badf98eb65836850a0ff66e2b1e2b8a6
Elio <eliomendonca115@gmail.com>
2026-07-27 00:30:31 +0000

    Aventureiro finalizado

--- xadrez.c
+++ xadrez.c
@@ -3,6 +3,7 @@
 int main() {
    int torre, rainha;
    int bispo = 1;
+   int cavalo = 1;
 
    rainha = 1;
 
@@ -10,19 +11,35 @@ int main() {
     printf("Torre moveu-se para direita\n");
    }
     
+printf("\n");
+
    while (bispo <= 5) {
     printf("Bispo moveu-se para cima\n");
     printf("Bispo moveu-se para direita\n");
     bispo++;
    }
 
+printf("\n");
+
    do {
     printf("Rainha moveu-se para esquerda\n");
     rainha++;
    } while (rainha <= 8);
 
+   printf("\n");
+
+   for (cavalo = 1; cavalo < 3; cavalo++) {
+      printf("Cavalo moveu-se para baixo\n");
+      printf("Cavalo moveu-se para baixo\n");
+       do {
+        printf("Cavalo moveu-se para esquerda\n");
+        cavalo++;
+    } while (cavalo > 2);
+   }
+
+
+
 
 return 0;
-
 
 }
